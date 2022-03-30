dc9d8af514882889aed0961e803d62b08ef5b87e
John Mwangi <jcmuthoni@safaricom.co.ke>
2022-03-30 07:44:49 +0300

    JPA repository

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -8,12 +9,15 @@ import java.util.List;
 
 @Service // Could also be @Component
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
-        return List.of(
-                new Student(1L,
-                        "John",
-                        "demo@gmail.com",
-                        22, LocalDate.of(2000, Month.NOVEMBER,1) )
-        );
+        return studentRepository.findAll();
     }
 }
