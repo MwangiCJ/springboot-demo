bd9576e444523d5c0a4705724271f1582ead068c
John Mwangi <jcmuthoni@safaricom.co.ke>
2022-03-31 14:32:56 +0300

    @Transactional

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,9 +3,11 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service // Could also be @Component
@@ -37,5 +39,35 @@ public class StudentService {
             throw new IllegalStateException("Student with id "+studentId+" does not exists.");
 
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Student student) {
+        Optional<Student> studentInstance = studentRepository.findById(student.getId());
+        if(!studentInstance.isPresent())
+            throw new IllegalStateException("Student was not found for update.");
+        studentInstance.get().setEmail(student.getEmail()); //todo: check if email belongs to another student
+        studentInstance.get().setName(student.getName());
+
+
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId).orElseThrow(()-> new IllegalStateException(
+                "Student with id "+ studentId + " does not exists."
+        ));
+
+        if(name !=null && name.length() > 0 && !Objects.equals(student.getName(), name))
+            student.setName(name);
+
+        if(email !=null && email.length()>0 && !Objects.equals(student.getEmail(), email)){
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if(studentOptional.isPresent())
+                throw new IllegalStateException("Email "+ email +" is already take.");
+
+            student.setEmail(email);
+        }
+
     }
 }
